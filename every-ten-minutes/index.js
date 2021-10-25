037bfa2e82326e98d59e6c436c574e1e061ee2b6
Sergio Linero <SLineroDev@gmail.com>
2021-10-25 08:27:44 +0200

    Added PCComponentes

--- every-ten-minutes/index.js
+++ every-ten-minutes/index.js
@@ -47,6 +47,16 @@ const shops = [
       const content = await page.textContent('#js_add_to_cart_desktop')
       return content.includes('Agotado temporalmente') === false
     }
+  },
+  {
+    vendor: 'PCComponentes',
+    hasSchema: true,
+    url: 'https://www.pccomponentes.com/microsoft-xbox-series-x-1tb',
+    checkStock: async ({ page }) => {
+      const content = await page.textContent('#buy-buttons-section')
+      console.log(content)
+      return content && content.includes('Añadir al carrito') === true
+    }
   }
   // disabled for now because it's not working properly
   // {
